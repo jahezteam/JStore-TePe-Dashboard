c053ee9e02af80068bf4c7677e0d19ba7ddca616
mohamed hamdy <opgghamdy@gmail.com>
2024-12-12 23:27:18 +0200

    update roles

--- src/app/pages/shared-module/Services/pick-list.service.ts
+++ src/app/pages/shared-module/Services/pick-list.service.ts
@@ -3,53 +3,72 @@ import { ApiCallerService } from './api-caller.service';
 import { environment } from '../../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PickListService {
-
-  constructor(private caller: ApiCallerService) {
-
-  }
+  constructor(private caller: ApiCallerService) {}
   apiUrl: string = environment.apiUrl;
+  getRegions() {
+    return this.caller.GetWithFullUrl(this.apiUrl + '/Region/GetLookup');
+  }
   getMainCategories() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/Product/GetMainCategoryLookup");
+    return this.caller.GetWithFullUrl(
+      this.apiUrl + '/Product/GetMainCategoryLookup',
+    );
   }
   getFeatureTypes() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/FeatureType/GetLookup");
+    return this.caller.GetWithFullUrl(this.apiUrl + '/FeatureType/GetLookup');
   }
   getgroups() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/ImageGroup/GetGroupsLookup");
+    return this.caller.GetWithFullUrl(
+      this.apiUrl + '/ImageGroup/GetGroupsLookup',
+    );
   }
   getTypes() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/types/getlist");
+    return this.caller.GetWithFullUrl(this.apiUrl + '/types/getlist');
   }
   getLevels() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/levels/getlist");
+    return this.caller.GetWithFullUrl(this.apiUrl + '/levels/getlist');
   }
   getPermissions() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/Role/GetPermissionsLookup");
+    return this.caller.GetWithFullUrl(
+      this.apiUrl + '/Role/GetPermissionsLookup',
+    );
   }
   getRoles() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/Role/GetRolesLookup");
+    return this.caller.GetWithFullUrl(this.apiUrl + '/Role/GetRolesLookup');
   }
   getUserRoles(id: any) {
-    return this.caller.GetByIdQuesry(this.apiUrl + "/User/UerRoles?UserId=" + id);
+    return this.caller.GetByIdQuesry(
+      this.apiUrl + '/User/UerRoles?UserId=' + id,
+    );
   }
 
   getQuestionLookups() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/QuestionBank/GetQuestionBankLookups");
+    return this.caller.GetWithFullUrl(
+      this.apiUrl + '/QuestionBank/GetQuestionBankLookups',
+    );
   }
   getPackageLookups() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/Product/GetProductLookups");
+    return this.caller.GetWithFullUrl(
+      this.apiUrl + '/Product/GetProductLookups',
+    );
   }
   getPackageLookupsForCoupons(couponId: number, type: number) {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/Product/getProductLookupsForCoupons/" + couponId + "/" + type);
+    return this.caller.GetWithFullUrl(
+      this.apiUrl +
+        '/Product/getProductLookupsForCoupons/' +
+        couponId +
+        '/' +
+        type,
+    );
   }
   getUserLookups() {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/User/GetUserLookups");
+    return this.caller.GetWithFullUrl(this.apiUrl + '/User/GetUserLookups');
   }
   getUserLookupsForPackage(pid: number, type: number) {
-    return this.caller.GetWithFullUrl(this.apiUrl + "/User/getUserLookupsForPackage/" + pid + "/" + type);
+    return this.caller.GetWithFullUrl(
+      this.apiUrl + '/User/getUserLookupsForPackage/' + pid + '/' + type,
+    );
   }
 }
-
