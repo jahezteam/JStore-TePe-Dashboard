3aa8385a8790899213f9b3da1a9d717e50b74aaa
Mohamed123Hamed <mo7hamed17698@gmail.com>
2024-12-19 01:39:27 +0200

    update environmnetProduct

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,13 @@
-
 export const environment = {
   SuperAdminData: {
-    Email: "admin",
-    Password: "P@ssw0rd"
+    Email: 'admin',
+    Password: 'P@ssw0rd',
   },
   production: false,
   appVersion: 'v8.0.35',
-  returnedUrl: "https://adminjstore.jahezteam.com.sa/",
+  returnedUrl: 'https://adminjstore.jahezteam.com.sa/',
   apiUrl: 'https://apijstore.jahezteam.com.sa/api',
+  imageUrl: 'https://apijstore.jahezteam.com.sa/',
   pageSize: 10,
-  feesPercentage:15
-
+  feesPercentage: 15,
 };
-
