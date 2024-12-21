9431828ef9845b3e97ba5c7f1748787c635c1623
mohamed hamdy <opgghamdy@gmail.com>
2024-12-21 22:03:39 +0200

    update style and change lang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   Inject,
   OnInit,
+  Renderer2,
 } from '@angular/core';
 import { TranslationService } from './modules/i18n';
 // language list
@@ -25,6 +26,7 @@ export class AppComponent implements OnInit {
     private translationService: TranslationService,
     public loadingService: LoaderServiceService,
     @Inject(DOCUMENT) private doc: Document,
+    private renderer: Renderer2,
   ) {
     // register translations
     this.translationService.loadTranslations(enLang, arLang);
@@ -35,17 +37,19 @@ export class AppComponent implements OnInit {
   }
 
   handleLanguageChange(lang: string) {
+    const htmlElement = this.renderer.selectRootElement('html', true);
+
     if (lang === 'ar') {
-      document.body.setAttribute('dir', 'rtl');
-      document.body.classList.add('rtl');
+      this.renderer.setAttribute(htmlElement, 'dir', 'rtl');
+      this.renderer.addClass(htmlElement, 'rtl');
       const changeStyle = this.doc.getElementById('switch-style');
       if (changeStyle) {
         changeStyle.setAttribute('href', './assets/css/rtl.css');
       }
     }
     if (lang === 'en') {
-      document.body.setAttribute('dir', 'ltr');
-      document.body.classList.remove('rtl');
+      this.renderer.setAttribute(htmlElement, 'dir', 'ltr');
+      this.renderer.addClass(htmlElement, 'ltr');
       const changeStyle = this.doc.getElementById('switch-style');
       if (changeStyle) {
         changeStyle.setAttribute('href', './assets/css/ltr.css');
